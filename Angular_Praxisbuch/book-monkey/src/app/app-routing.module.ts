c5a7abcab21384191a769adc50f8da34a1e3c0a5
Albert Blarer <ablarer@gmx.ch>
2023-08-31 15:47:42 +0200

    End of chapter 25.09

--- Angular_Praxisbuch/book-monkey/src/app/app-routing.module.ts
+++ Angular_Praxisbuch/book-monkey/src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
   },
   {
     path: 'books',
-    loadChildren: () => import('./books/books.module').then(m => m.BooksModule)
+    loadChildren: () => import('./books/books.routes').then(m => m.BOOKS_ROUTES)
   },
   {
     path: 'admin',
